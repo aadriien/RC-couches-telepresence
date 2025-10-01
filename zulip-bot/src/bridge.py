8d869ad64b81e7671127d9f1bff8f4921391984a
aadriien <adriienlynch@gmail.com>
2025-10-01 19:01:32 -0400

    Established pipeline for Zulip bot to respond to messages & take action

--- zulip-bot/src/bridge.py
+++ zulip-bot/src/bridge.py
@@ -5,6 +5,9 @@
 ###############################################################################
 
 
+from src.notifier import send_request_succeeded, send_request_failed
+
+
 def process_request(msg_event, client):
     if msg_event["type"] != "message":
         return
@@ -27,5 +30,29 @@ def process_request(msg_event, client):
 
         print(msg_event)
         print(msg["content"])
+
+        parse_message(msg, client)
+
+
+def parse_message(msg, client):
+    VALID_PROMPTS = [
+        "open the portal",
+        "open the couch portal",
+        "open the bridge",
+        "open the couch bridge"
+    ]
+
+    # Tag person who requested bridge to notify them of result
+    user_to_mention = msg["sender_full_name"]
+    mention_markdown = f"@**{user_to_mention}**"
+
+    curr_stream_id, curr_subject = msg["stream_id"], msg["subject"]
+
+    content = msg["content"].lower()
+
+    if not any(prompt in content for prompt in VALID_PROMPTS):
+        send_request_failed(mention_markdown, curr_stream_id, curr_subject, client)
+    else:
+        send_request_succeeded(mention_markdown, curr_stream_id, curr_subject, client)
 
 
--- zulip-bot/src/notifier.py
+++ zulip-bot/src/notifier.py
@@ -5,20 +5,65 @@
 ###############################################################################
 
 
-STREAM_ID = "🧑‍💻 current batches" # channel (e.g. checkins)
-SUBJECT = "Virtual couches co-working" # topic (e.g. FirstName LastName)
+# NOTICE_STREAM_ID = "🧑‍💻 current batches" # channel (e.g. checkins)
+# NOTICE_SUBJECT = "Virtual couches co-working" # topic (e.g. FirstName LastName)
+
+# HUB_STREAM_ID = "397 Bridge"
+# HUB_SUBJECT = "RCTV Couches Telepresence"
+
+
+# TESTING FOR NOW
+NOTICE_STREAM_ID = "test-bot" 
+NOTICE_SUBJECT = "RC-couches-telepresence-bridge" 
+
+HUB_STREAM_ID = "test-bot"
+HUB_SUBJECT = "RC-couches-telepresence-bridge"
+
+
 ZOOM_LINK = "https://www.recurse.com/zoom/couches"
+COUCHES_ACTIVE_NOTICE = f"Couch bridge is active! Join the zoom call: {ZOOM_LINK}"
 
 
-def send_notification(client):
-    couches_active_notice = f"Couch bridge is active! Join the zoom call: {ZOOM_LINK}"
+def send_notification(notification_msg, stream_id, subject, client):
 
     # Send a channel message
     client.send_message({
         "type": "stream",
-        "to": STREAM_ID,
-        "topic": SUBJECT,
-        "content": couches_active_notice
+        "to": stream_id,
+        "topic": subject,
+        "content": notification_msg
     })
 
+
+def send_request_succeeded(mention_markdown, curr_stream_id, curr_subject, client):
+    hub_request_link = f"#**{HUB_STREAM_ID}>{HUB_SUBJECT}** "
+
+    REQUEST_SUCCEEDED = (
+        f"{mention_markdown} Great! Thanks for pinging me. "
+        f"I'll send a request to folks at the hub over in {hub_request_link}"
+    )
+
+    send_notification(REQUEST_SUCCEEDED, curr_stream_id, curr_subject, client)
+
+    # Prepare request to in-person hub folks (397 Bridge channel)
+    mention_topic = "@**topic**"
+    
+    REQUEST_TO_HUB = (
+        f"{mention_topic} Hey hub folks! "
+        "The remote RCers would like to open the couches bridge for telepresence. "
+        "Can you help them out? Activate the Zoom call on RCTV!"
+    )
+
+    send_notification(REQUEST_TO_HUB, HUB_STREAM_ID, HUB_SUBJECT, client)
+    
+
+def send_request_failed(mention_markdown, curr_stream_id, curr_subject, client):
+    REQUEST_FAILED = (
+        f"{mention_markdown} Sorry, I don't understand. "
+        "If you would like to request a bridge with the hub couches, say "
+        "\"Please open the portal\", or \"Please open the bridge\".\n\n"
+        "Don't forget to tag me!"
+    )
+
+    send_notification(REQUEST_FAILED, curr_stream_id, curr_subject, client)
 
